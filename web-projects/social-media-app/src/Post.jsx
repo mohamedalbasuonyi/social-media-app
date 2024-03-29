cb231723605b8bd3b0965fe1aff1f657492ec035
Mohamed Albasuony <97564022+mohamedalbasuony1@users.noreply.github.com>
2024-03-29 15:55:26 -0400

    .

--- web-projects/social-media-app/src/Post.jsx
+++ web-projects/social-media-app/src/Post.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
-function Post() {
-  return (
-    <div>
-      <h3>Test Post</h3>
-      <p>This is a test post we'll edit later!</p>
-      <p>Likes: 30</p>
-      <p>Comments:</p>
-    </div>
-  );
+import Comment from './Comment';
+function Post(){
+return(
+<div>
+<h3>Test Post</h3>
+<p>This is a test post we'll edit later!</p>
+<p>Likes: 30</p>
+<p>Comments:</p>
+<Comment />
+</div>
+)
 }
-
-export default Post;
+export default Post
