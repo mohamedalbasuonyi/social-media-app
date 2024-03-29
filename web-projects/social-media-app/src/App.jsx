12c162f501c505499a730fa6f0d0b8e587b170d0
Mohamed Albasuony <97564022+mohamedalbasuony1@users.noreply.github.com>
2024-03-29 15:50:47 -0400

    created post component

--- web-projects/social-media-app/src/App.jsx
+++ web-projects/social-media-app/src/App.jsx
@@ -4,16 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import React from 'react';
+import Post from '/Users/mohamedalbasuony/web-projects/social-media-app/src/Post.jsx'; 
 
-function App(){
-return(
-<div>
-<h1>Fakebook!</h1>
-{/* CreatePostForm */}
-{/* Feed */}
-{/* Post */}
-</div>
-)
+function App() {
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      {/* CreatePostForm */}
+      {/* Feed */}
+      <Post /> {/* Render the Post component here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
