e0a410ea35ca1e3bd29ec7410fad552a10fd2fed
Mohamed Albasuony <97564022+mohamedalbasuony1@users.noreply.github.com>
2024-04-03 17:49:02 -0400

    Added the option to add comments to the posts

--- Downloads/web-projects/social-media-app/src/Post.jsx
+++ Downloads/web-projects/social-media-app/src/Post.jsx
@@ -3,9 +3,20 @@ import Comment from './Comment';
 
 function Post(props) {
   const [likes, setLikes] = useState(0);
+  const [comments, setComments] = useState([]);
 
   function handleLike() {
     setLikes(likes + 1);
+  }
+
+  function handleCommentSubmit(e) {
+    e.preventDefault();
+
+    const comment = e.target.elements.comment.value;
+
+    setComments([...comments, comment]);
+
+    e.target.reset();
   }
 
   return (
@@ -16,6 +27,18 @@ function Post(props) {
       <button onClick={handleLike}>Like</button>
       <p>Comments:</p>
 
+      <ul>
+        {comments.map((comment, index) => (
+          <Comment key={index} content={comment} />
+        ))}
+      </ul>
+
+      <form onSubmit={handleCommentSubmit}>
+        <label htmlFor="comment">Comment:</label>
+        <input type="text" id="comment" />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
